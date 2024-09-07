725d4e6f75706ff10e6bbb312cccf06789db1dec
quinn <qda869@usask.ca>
2024-09-07 09:30:58 -0600

    Implemented checkeredground scene

--- src/Scenes.hs
+++ src/Scenes.hs
@@ -1,5 +1,5 @@
 module Scenes where
-  
+
 import           Data.Foldable (Foldable (foldl'))
 import           Hittable      (Material (..), SomeHittable (SomeHittable))
 import           Random        (arbitraryVec3, arbitraryVec3InRange,
@@ -7,12 +7,15 @@ import           Random        (arbitraryVec3, arbitraryVec3InRange,
 import           Ray           (Ray (..))
 import           Sphere        (Sphere (..))
 import           System.Random (StdGen, mkStdGen)
+import           Texture       (SolidColour (SolidColour),
+                                SomeTexture (SomeTexture),
+                                checkerTextureFromColours)
 import           Vec3          (Vec3 (..), magnitude, (^*^), (^-^))
 
 staticBalls :: Int -> [SomeHittable]
 staticBalls seed = ground ++ fixed ++ randomObjects
   where
-    ground = [SomeHittable $ StaticSphere (Vec3 0 (-1000) 0) 1000 (Lambertian (Vec3 0.5 0.5 0.5))]
+    ground = [SomeHittable $ StaticSphere (Vec3 0 (-1000) 0) 1000 (Lambertian . SomeTexture $ SolidColour (Vec3 0.5 0.5 0.5))]
 
     randomObjects = fst $ foldl' makeObject ([], mkStdGen seed) coordinates
       where
@@ -35,21 +38,21 @@ staticBalls seed = ground ++ fixed ++ randomObjects
                 (d1, _) = sampleFraction g5
                 col = p1 ^*^ p2
                 mat
-                  | chooseMat < 0.8 = Lambertian col
+                  | chooseMat < 0.8 = Lambertian . SomeTexture $ SolidColour col
                   | chooseMat < 0.95 = Metal (Vec3 (0.5 + 0.5 * x1) (0.5 + 0.5 * y1) (0.5 + 0.5 * z1)) (0.5 * d1)
                   | otherwise = Dialectric 1.5
                   where Vec3 x1 y1 z1 = p1
                 obj = StaticSphere center 0.2 mat
 
     fixed = [ SomeHittable $ StaticSphere (Vec3 0 1 0) 1 (Dialectric 1.5)
-            , SomeHittable $  StaticSphere (Vec3 (-4) 1 0) 1 (Lambertian (Vec3 0.4 0.2 0.1))
+            , SomeHittable $ StaticSphere (Vec3 (-4) 1 0) 1 (Lambertian . SomeTexture $ SolidColour (Vec3 0.4 0.2 0.1))
             , SomeHittable $ StaticSphere (Vec3 4 1 0) 1 (Metal (Vec3 0.7 0.6 0.5) 0)
             ]
 
 bouncingBalls :: Int -> [SomeHittable]
 bouncingBalls seed = ground ++ fixed ++ randomObjects
   where
-    ground = [SomeHittable $ StaticSphere (Vec3 0 (-1000) 0) 1000 (Lambertian (Vec3 0.5 0.5 0.5))]
+    ground = [SomeHittable $ StaticSphere (Vec3 0 (-1000) 0) 1000 (Lambertian . SomeTexture $ SolidColour (Vec3 0.5 0.5 0.5))]
 
     randomObjects = fst $ foldl' makeObject ([], mkStdGen seed) coordinates
       where
@@ -73,7 +76,7 @@ bouncingBalls seed = ground ++ fixed ++ randomObjects
                 col = p1 ^*^ p2
                 displacement = Vec3 0.0 (fst (sampleFractionInRange g6 0.0 0.5)) 0.0
                 mat
-                  | chooseMat < 0.8 = Lambertian col
+                  | chooseMat < 0.8 = Lambertian .  SomeTexture $ SolidColour col
                   | chooseMat < 0.95 = Metal (Vec3 (0.5 + 0.5 * x1) (0.5 + 0.5 * y1) (0.5 + 0.5 * z1)) (0.5 * d1)
                   | otherwise = Dialectric 1.5
                   where Vec3 x1 y1 z1 = p1
@@ -83,6 +86,48 @@ bouncingBalls seed = ground ++ fixed ++ randomObjects
 
 
     fixed = [ SomeHittable $ StaticSphere (Vec3 0 1 0) 1 (Dialectric 1.5)
-            , SomeHittable $ StaticSphere (Vec3 (-4) 1 0) 1 (Lambertian (Vec3 0.4 0.2 0.1))
+            , SomeHittable $ StaticSphere (Vec3 (-4) 1 0) 1 (Lambertian . SomeTexture $ SolidColour (Vec3 0.4 0.2 0.1))
+            , SomeHittable $ StaticSphere (Vec3 4 1 0) 1 (Metal (Vec3 0.7 0.6 0.5) 0)
+            ]
+
+bouncingBallsCheckered :: Int -> [SomeHittable]
+bouncingBallsCheckered seed = ground ++ fixed ++ randomObjects
+  where
+    checker = checkerTextureFromColours 0.32 (Vec3 0.2 0.3 0.1) (Vec3 0.9 0.9 0.9)
+    ground  = [SomeHittable $ StaticSphere (Vec3 0 (-1000) 0) 1000 (Lambertian $ SomeTexture checker)]
+
+    randomObjects = fst $ foldl' makeObject ([], mkStdGen seed) coordinates
+      where
+        coordinates = [(a, b) | a <- [-11..11], b <- [-11..11]]
+
+        makeObject :: ([SomeHittable], StdGen) -> (Int, Int) -> ([SomeHittable], StdGen)
+        makeObject (objects, g) (a, b) = (maybe objects (: objects) object, g2)
+          where
+            (Vec3 chooseMat x z, g1) = arbitraryVec3InRange 0 1 g
+            center = Vec3 (fromIntegral a + 0.9 * x) 0.2 (fromIntegral b + 0.9 * z)
+            (object, g2)
+              | magnitude (center ^-^ Vec3 4 0.2 0) > 0.9 = createRandomObject g1
+              | otherwise = (Nothing, g1)
+
+            createRandomObject :: StdGen -> (Maybe SomeHittable, StdGen)
+            createRandomObject g3 = (Just (SomeHittable obj), g5)
+              where
+                (p1, g4) = arbitraryVec3 g3
+                (p2, g5) = arbitraryVec3 g4
+                (d1, g6) = sampleFraction g5
+                col = p1 ^*^ p2
+                displacement = Vec3 0.0 (fst (sampleFractionInRange g6 0.0 0.5)) 0.0
+                mat
+                  | chooseMat < 0.8 = Lambertian .  SomeTexture $ SolidColour col
+                  | chooseMat < 0.95 = Metal (Vec3 (0.5 + 0.5 * x1) (0.5 + 0.5 * y1) (0.5 + 0.5 * z1)) (0.5 * d1)
+                  | otherwise = Dialectric 1.5
+                  where Vec3 x1 y1 z1 = p1
+                obj
+                  | chooseMat < 0.8 = MovingSphere (Ray center displacement 0.0) 0.2 mat
+                  | otherwise       = StaticSphere center 0.2 mat
+
+
+    fixed = [ SomeHittable $ StaticSphere (Vec3 0 1 0) 1 (Dialectric 1.5)
+            , SomeHittable $ StaticSphere (Vec3 (-4) 1 0) 1 (Lambertian . SomeTexture $ SolidColour (Vec3 0.4 0.2 0.1))
             , SomeHittable $ StaticSphere (Vec3 4 1 0) 1 (Metal (Vec3 0.7 0.6 0.5) 0)
             ]
--- src/Texture.hs
+++ src/Texture.hs
@@ -7,7 +7,7 @@ import           Vec3 (RGB, Vec3(..))
 class Texture t where
     value :: t -> (R, R) -> Vec3 -> RGB
 
-data SomeTexture = forall t. Texture t => SomeTexture t 
+data SomeTexture = forall t. Texture t => SomeTexture t
 
 newtype SolidColour = SolidColour RGB
 
@@ -15,7 +15,7 @@ data CheckerTexture = CheckerTexture { invScale    :: R
                                      , evenTexture :: SomeTexture
                                      , oddTexture  :: SomeTexture}
 
-instance Texture SomeTexture where 
+instance Texture SomeTexture where
     value (SomeTexture tex) = value tex
 
 instance Texture SolidColour where
@@ -23,11 +23,17 @@ instance Texture SolidColour where
 
 instance Texture CheckerTexture where
     value (CheckerTexture scale oddT evenT) (u, v) p =
-        let xInt   = floor (scale * xComp p) :: Int 
-            yInt   = floor (scale * yComp p) :: Int 
-            zInt   = floor (scale * zComp p) :: Int 
+        let xInt   = floor (scale * xComp p) :: Int
+            yInt   = floor (scale * yComp p) :: Int
+            zInt   = floor (scale * zComp p) :: Int
             isEven = even (xInt + yInt + zInt)
         in if isEven
             then value evenT (u, v) p
             else value oddT (u, v) p
 
+checkerTexture :: R -> SomeTexture -> SomeTexture -> CheckerTexture
+checkerTexture scale = CheckerTexture (1.0 / scale)
+
+checkerTextureFromColours :: R -> RGB -> RGB -> CheckerTexture 
+checkerTextureFromColours scale c1 c2 = 
+    checkerTexture scale (SomeTexture $ SolidColour c1) (SomeTexture $ SolidColour c2)
