b0fc04ee70a8b44db2c45732ffd38bb903c92cf2
quinn <qda869@usask.ca>
2024-09-17 19:15:01 -0600

    Formatting

--- src/Perlin.hs
+++ src/Perlin.hs
@@ -27,10 +27,10 @@ import           Math          (R)
 import           System.Random (Random (..), RandomGen, mkStdGen)
 import           Vec3          (Vec3 (..))
 
-data Perlin = Perlin { randFloats :: !(Vector R)
-                     , permX      :: !(Vector Int)
-                     , permY      :: !(Vector Int)
-                     , permZ      :: !(Vector Int)
+data Perlin = Perlin { randFloats :: Vector R
+                     , permX      :: Vector Int
+                     , permY      :: Vector Int
+                     , permZ      :: Vector Int
                      }
 
 pointCount :: Int
@@ -62,8 +62,8 @@ perlinNoise perlin (Vec3 x y z) =
         !w = z - fromIntegral zFloor
     -- Collect the corner values from the grid into a flat array
         c = V.generate 8 $ \idx ->
-              let di = (idx `shiftR` 2) .&. 1  -- Extract bit 2 (di)
-                  dj = (idx `shiftR` 1) .&. 1  -- Extract bit 1 (dj)
+              let di = idx `shiftR` 2 .&. 1  -- Extract bit 2 (di)
+                  dj = idx `shiftR` 1 .&. 1  -- Extract bit 1 (dj)
                   dk = idx .&. 1               -- Extract bit 0 (dk)
               in randFloats perlin V.! ((permX perlin V.! ((i + di) .&. 255))
                              `xor` (permY perlin V.! ((j + dj) .&. 255))
@@ -73,8 +73,8 @@ perlinNoise perlin (Vec3 x y z) =
 -- Trilinear interpolation of a 2x2x2 grid cube of values based on fractional coordinates (u, v, w).
 trilinearInterp :: Vector R -> (R, R, R) -> R
 trilinearInterp c (u, v, w) =
-    let accum = sum [ let !i' = fromIntegral ((idx `shiftR` 2) .&. 1)
-                          !j' = fromIntegral ((idx `shiftR` 1) .&. 1)
+    let accum = sum [ let !i' = fromIntegral (idx `shiftR` 2 .&. 1)
+                          !j' = fromIntegral (idx `shiftR` 1 .&. 1)
                           !k' = fromIntegral (idx .&. 1)
                       in (i' * u + (1 - i') * (1 - u))
                        * (j' * v + (1 - j') * (1 - v))
